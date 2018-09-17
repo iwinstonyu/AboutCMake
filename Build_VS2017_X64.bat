235932d746331b5a20ab73001f2d5927cc53f4e8
windpenguin <windpenguin@gmail.com>
2018-09-17 23:42:52 +0800

    no hello sln

--- Build_VS2017_X64.bat
+++ Build_VS2017_X64.bat
@@ -1,7 +1,8 @@
 @echo off
 
-mkdir build\vs2017
-cd build\vs2017
+if exist build\vs2017_64 rd /s /q build\vs2017_64
+md build\vs2017_64
+cd build\vs2017_64
 cmake ../.. -G "Visual Studio 15 2017 Win64"
 cd ../..
 
